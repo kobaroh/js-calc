0fbbfd57aeef0608ef9728b346307e9c42bb70c5
kobaroh <61962552+kobaroh@users.noreply.github.com>
2022-06-24 23:41:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var num1 = window.prompt("Enter a number");
+var num2 = window.prompt("Enter another number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2); 
+
+DocumentTimeline.write(num1 + num2);
